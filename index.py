85548185050dd90b2cfe82bbc2fdb43acbc8aa4e
mohamed chejjar <mchejjar@yahoo.com>
2024-07-03 12:01:50 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,120 @@
+Cars = [
+    {
+        "ID": 1, 
+        "Make": "Toyota", 
+        "Model": "Corolla", 
+        "year": 2022
+    },
+    {
+        "ID": 2, 
+        "Make": "Honda", 
+        "Model": "Civic", 
+        "year": 2021
+    },
+    {
+        "ID": 3, 
+        "Make": "Ford", 
+        "Model": "Focus", 
+        "year": 2020
+    },
+    {
+        "ID": 4, 
+        "Make": "Chevrolet", 
+        "Model": "Cruze", 
+        "year": 2023
+    },
+    {
+        "ID": 5, 
+        "Make": "Volkswagen", 
+        "Model": "Golf", 
+        "year": 2022
+    },
+    {
+        "ID": 6, 
+        "Make": "Nissan", 
+        "Model": "Altima", 
+        "year": 2021
+    },
+    {
+        "ID": 7, 
+        "Make": "Hyundai", 
+        "Model": "Elantra", 
+        "year": 2020
+    },
+    {
+        "ID": 8, 
+        "Make": "Mazda", 
+        "Model": "Mazda3", 
+        "year": 2023
+    },
+    {
+        "ID": 9, 
+        "Make": "Subaru", 
+        "Model": "Impreza", 
+        "year": 2022
+    },
+    {
+        "ID": 10, 
+        "Make": "Kia", 
+        "Model": "Forte", 
+        "year": 2021
+    },
+    {
+        "ID": 11, 
+        "Make": ke"Audi", 
+        "Model": "A4", 
+        "year": 2020
+    },
+    {
+        "ID": 12, 
+        "Make": "BMW", 
+        "Model": "3 Series", 
+        "year": 2023
+    },
+    {
+        "ID": 13, 
+        "Make": "Mercedes-Benz", 
+        "Model": "C-Class", 
+        "year": 2022
+    },
+    {
+        "ID": 14, 
+        "Make": "Lexus", 
+        "Model": "IS", 
+        "year": 2021
+    },
+    {
+        "ID": 15, 
+        "Make": "Jaguar", 
+        "Model": "XE", 
+        "year": 2020
+    },
+    {
+        "ID": 16, 
+        "Make": "Tesla", 
+        "Model": "Model 3", 
+        "year": 2023
+    },
+    {
+        "ID": 17, 
+        "Make": "Porsche", 
+        "Model": "Panamera", 
+        "year": 2022
+    },
+    {
+        "ID": 18, 
+        "Make": "Ferrari", 
+        "Model": "488 GTB", 
+        "year": 2021
+    },
+    {
+        "ID": 19, 
+        "Make": "Lamborghini", 
+        "Model": "Huracan", 
+        "year": 2020
+    },
+    {
+        "ID": 20, 
+        "Make": "Bugatti", 
+        "Model": "Chiron", 
+        "year": 2023
